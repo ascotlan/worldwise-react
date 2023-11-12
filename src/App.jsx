7ab0827e07a0bb3f29b174c8d555768aa4c1222a
ascotlan <antonio.k.scotland@gmail.com>
2023-11-11 21:42:10 -0500

    Implement routing with ReactRouter

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNoteFound from "./pages/PageNoteFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNoteFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/PageNoteFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNoteFound() {
+  return (
+    <div>
+      <PageNav />
+      Page not found :(
+    </div>
+  );
+}
+
+export default PageNoteFound;
