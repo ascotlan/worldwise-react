093685ddc409479c37b93e8517ceb08e22f4331e
ascotlan <antonio.k.scotland@gmail.com>
2024-01-18 11:07:43 -0500

    Implement components that change the map positiona nd detect a click on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,35 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useState, useEffect } from "react";
 import useCitiesContext from "../hooks/use-cities-context";
+import PropTypes from "prop-types";
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCitiesContext();
-  const [mapPosition] = useState([40, 0]);
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  const [searchParams] = useSearchParams();
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -31,9 +47,32 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
+  return null;
+}
+
+ChangeCenter.propTypes = {
+  position: PropTypes.array.isRequired,
+};
 
 export default Map;
