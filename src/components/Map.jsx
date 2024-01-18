1f121788cc69c0adef00f19da69577df807ea78a
ascotlan <antonio.k.scotland@gmail.com>
2024-01-18 10:35:09 -0500

    Implement the React-leaflet and leaflet libraries to display the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,39 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import styles from './Map.module.css'
+import { useNavigate } from "react-router-dom";
+import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import useCitiesContext from "../hooks/use-cities-context";
 
 function Map() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const { cities } = useCitiesContext();
+  const [mapPosition] = useState([40, 0]);
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position : {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({lat:23, lng:50})}>Change pos</button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
